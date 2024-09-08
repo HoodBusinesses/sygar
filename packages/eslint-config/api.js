fee305ccbfe4ff91c60b04a65d2d420b36836264
Anas jaidi <anas.jaidi@icloud.com>
2024-09-08 17:35:53 +0100

    feat: shared self contained linting package

---
+++ packages/eslint-config/api.js
@@ -0,0 +1,40 @@
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "turbo" // Assuming you are using Turbo
+  ],
+  plugins: ["@typescript-eslint", "prettier", "only-warn"],
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  env: {
+    node: true,
+    jest: true,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    ".*.js",
+    "node_modules/",
+    "dist/",
+  ],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
