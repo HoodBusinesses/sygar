642ecf499c0360a83bf44f94a7a501bfc2b07546
Marouan Doulahiane <98280741+MarouanDoulahiane@users.noreply.github.com>
2024-09-13 16:53:27 +0100

    Update auth.module.ts

--- apps/api/src/global/auth/auth.module.ts
+++ apps/api/src/global/auth/auth.module.ts
@@ -4,7 +4,7 @@ import { AuthController } from './auth.controller';
 @Global()
 @Module({
   imports: [],
-  controller: [AuthController],
+  controllers: [AuthController],
   exports: [],
   providers: [],
 })
