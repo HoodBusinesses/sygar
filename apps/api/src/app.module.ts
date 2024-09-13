0175400e047ff8a7b9dbd0187d2e278ac373b85e
Marouan Doulahiane <98280741+MarouanDoulahiane@users.noreply.github.com>
2024-09-13 16:55:38 +0100

    Update app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,18 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AuthModule } from './global/auth/auth.module';
-import { DbModule } from './global/db/db.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    AuthModule,
-    DbModule,
-    ConfigModule.forRoot(
-    {
-      isGlobal: true,
-    }
-  )],
+  imports: [AuthModule],
   controllers: [AppController],
 })
 export class AppModule {}
