23c730cd8cc4704a491366e9c3c0bf30c6e6f62a
PiPe <you@example.com>
2024-09-20 13:49:53 +0100

    merge dev branch into syg-4

---
+++ apps/desktop/renderer/tailwind.config.js
@@ -0,0 +1,23 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    './renderer/pages/**/*.{js,ts,jsx,tsx}',
+    './renderer/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    colors: {
+      // use colors only specified
+      white: colors.white,
+      gray: colors.gray,
+      blue: colors.blue,
+    },
+    extend: {
+      backgroundImage: {
+        'gradient-to-blue': 'linear-gradient(269.56deg, #2563EB -3.3%, #FFFFFF 101.39%)',
+      },
+
+    },
+  },
+  plugins: [],
+}
