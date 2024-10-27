64288e18e7c151c95e70bfc1810b793b40c86025
anas jaidi <anas.jaidi@icloud.com>
2024-10-27 20:24:50 +0100

    chore: add global layout container

---
+++ apps/desk/src/renderer/src/containers/layout.tsx
@@ -0,0 +1,24 @@
+import Navbar from "@renderer/components/Navbar";
+import Sidebar from "@renderer/components/Sidebar";
+import { useAuth } from "@renderer/hooks/useAuth";
+import { useProtocol } from '@renderer/hooks/useProtocol'
+import { Outlet } from "@tanstack/react-router";
+
+export const Layout = () => {
+  const auth = useAuth();
+  useProtocol();
+
+  return (
+    <>
+      <div className="flex flex-col h-full">
+        {auth.isAuth && <Navbar />}
+        <div className="flex-1 flex h-full">
+          {auth.isAuth && <Sidebar />}
+          <main className="flex-1 bg-white p-6">
+            <Outlet />
+          </main>
+        </div>
+      </div>
+    </>
+  )
+}
