aebba56e30b14a0e3e05c74933fee0a1640c3b0d
Hameur <hameur@Hameur.hameur>
2024-10-30 22:31:15 +0100

    refactor sideBar

--- apps/desk/src/renderer/src/components/Sidebar.tsx
+++ apps/desk/src/renderer/src/components/Sidebar.tsx
@@ -1,15 +1,8 @@
 'use client'
 import { useEffect, useState } from 'react'
-import {
-  Cog6ToothIcon,
-  BellIcon,
-  ClipboardDocumentListIcon,
-  HomeIcon,
-  UsersIcon,
-  IdentificationIcon
-} from '@heroicons/react/24/outline'
 import { useTranslation } from 'react-i18next'
 import { Link } from '@tanstack/react-router'
+import { navItems } from '@renderer/data/sidebar-items'
 // import { usePersistentStore } from '../contexts/PersistentStoreContext';
 
 export default function Sidebar() {
@@ -33,92 +26,27 @@ export default function Sidebar() {
           {t('sidebar.sections.dashboard')}
         </div>
         <ul className="space-y-2">
-          <li>
-            <Link href="/">
-              <a
+          {navItems.map((item) => (
+            <li key={item.name}>
+              <Link
+                href={item.href}
                 className={`flex items-center px-4 py-2 space-x-3 text-sm font-medium ${
-                  active === 'home' ? 'bg-blue-100 text-blue-500' : 'text-gray-600'
+                  active === item.name ? 'bg-blue-100 text-blue-500' : 'text-gray-600'
                 } hover:bg-blue-100 hover:text-blue-500`}
-                onClick={() => setActive('home')}
+                onClick={() => setActive(item.name)}
               >
-                <HomeIcon className="h-5 w-5" />
-                <span>{t('sidebar.items.home')}</span>
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/registration">
-              <a
-                className={`flex items-center px-4 py-2 space-x-3 text-sm font-medium ${
-                  active === 'registration' ? 'bg-blue-100 text-blue-500' : 'text-gray-600'
-                } hover:bg-blue-100 hover:text-blue-500`}
-                onClick={() => setActive('registration')}
-              >
-                <IdentificationIcon className="h-5 w-5" />
-                <span>{t('sidebar.items.registration')}</span>
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/organizations">
-              <a
-                className={`flex items-center px-4 py-2 space-x-3 text-sm font-medium ${
-                  active === 'organizations' ? 'bg-blue-100 text-blue-500' : 'text-gray-600'
-                } hover:bg-blue-100 hover:text-blue-500`}
-                onClick={() => setActive('organizations')}
-              >
-                <UsersIcon className="h-5 w-5" />
-                <span>{t('sidebar.items.organizations')}</span>
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/settings">
-              <a
-                className={`flex items-center px-4 py-2 space-x-3 text-sm font-medium ${
-                  active === 'settings' ? 'bg-blue-100 text-blue-500' : 'text-gray-600'
-                } hover:bg-blue-100 hover:text-blue-500`}
-                onClick={() => setActive('settings')}
-              >
-                <Cog6ToothIcon className="h-5 w-5" />
-                <span>{t('sidebar.items.settings')}</span>
-              </a>
-            </Link>
-          </li>
+                {item.icon && <item.icon className="h-5 w-5" />}
+                {t(item.label)}
+              </Link>
+            </li>
+          ))}
         </ul>
 
         {/* Othres Section */}
         <div className="px-4 py-2 mt-8 text-xs font-semibold text-gray-500 uppercase">
           {t('sidebar.sections.others')}
         </div>
-        <ul className="space-y-2">
-          <li>
-            <Link href="/themes-listing">
-              <a
-                className={`flex items-center px-4 py-2 space-x-3 text-sm font-medium ${
-                  active === 'notifications' ? 'bg-blue-100 text-blue-500' : 'text-gray-600'
-                } hover:bg-blue-100 hover:text-blue-500`}
-                onClick={() => setActive('notifications')}
-              >
-                <BellIcon className="h-5 w-5" />
-                <span>{t('sidebar.items.notifications')}</span>
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/participants-listing">
-              <a
-                className={`flex items-center px-4 py-2 space-x-3 text-sm font-medium ${
-                  active === 'reports' ? 'bg-blue-100 text-blue-500' : 'text-gray-600'
-                } hover:bg-blue-100 hover:text-blue-500`}
-                onClick={() => setActive('reports')}
-              >
-                <ClipboardDocumentListIcon className="h-5 w-5" />
-                <span>{t('sidebar.items.reports')}</span>
-              </a>
-            </Link>
-          </li>
-        </ul>
+        <ul className="space-y-2"></ul>
       </nav>
     </div>
   )
