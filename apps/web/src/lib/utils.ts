53cd95207abfafecabd5118444ba46965c75a64b
Merouane Grissen <mgrissen9@gmail.com>
2024-11-14 19:40:38 +0100

    chore: create func for extract token from header

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+
+export const extractTokenFromHeader = (authorizationHeader: string | undefined): string | null => {
+  if (!authorizationHeader) {
+    return null;
+  }
+
+  const parts = authorizationHeader.split(' ');
+  if (parts.length === 2 && parts[0] === 'Bearer') {
+    return parts[1];
+  }
+
+  return null;
+};
+
