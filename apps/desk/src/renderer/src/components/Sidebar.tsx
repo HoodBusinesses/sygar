6647c35654e490f46d4e0cbe7dc365d6e6c24320
Said mia <saidmia.dev@gmail.com>
2024-12-10 15:30:48 +0100

    fix highlight active route

--- apps/desk/src/renderer/src/components/Sidebar.tsx
+++ apps/desk/src/renderer/src/components/Sidebar.tsx
@@ -1,11 +1,11 @@
 import { navItems } from '@renderer/data/sidebar-items';
 import { useAppSelector } from '@renderer/store/hooks';
-import { Link } from '@tanstack/react-router';
-import { useState } from 'react';
+import { Link, useLocation } from '@tanstack/react-router';
 import { useTranslation } from 'react-i18next';
+import { cn } from './ui/lib/utils';
 
 export default function Sidebar() {
-  const [active, setActive] = useState('home');
+  const location = useLocation();
 
   const { t } = useTranslation();
 
@@ -16,6 +16,8 @@ export default function Sidebar() {
       userProfile: state.auth.auth.userType,
     })
   );
+
+  const active = location.pathname;
 
   return (
     <div className="h-screen w-64 bg-gray-50 border-r border-gray-200 p-4">
@@ -28,24 +30,25 @@ export default function Sidebar() {
         <ul className="flex flex-col gap-2">
           {navItems(isAccountActivated, userRole, userProfile)
             .filter((item) => item.canVue)
-            .map((item) => (
-              <li key={item.name}>
+            .map((item, index) => (
+              <li key={index}>
                 <Link
                   href={item.href}
-                  className={`flex items-center px-4 py-2 gap-3 rounded-md text-sm font-poppins font-normal leading-[12px] tracking-[0.5px] transition-colors duration-200 ${
-                    active === item.name
+                  className={cn(
+                    'flex items-center px-4 py-2 gap-3 rounded-md text-sm font-poppins font-normal leading-[12px] tracking-[0.5px] transition-colors duration-200',
+                    active.includes(item.href)
                       ? 'bg-blue-100/50 text-blue-600'
                       : 'text-[#273240] hover:bg-blue-50 hover:text-blue-500'
-                  }`}
-                  onClick={() => setActive(item.name)}
+                  )}
                 >
                   {item.icon && (
                     <item.icon
-                      className={`h-5 w-5 ${
-                        active === item.name
-                          ? 'bg-blue-100/50 text-[#2563E9] '
+                      className={cn(
+                        'h-5 w-5',
+                        active.includes(item.href)
+                          ? 'bg-blue-100/50 text-[#2563E9]'
                           : 'text-[#1E3A8A59] hover:bg-blue-50 hover:text-blue-500'
-                      }`}
+                      )}
                     />
                   )}
                   <span>{t(item.label)}</span>
@@ -58,27 +61,6 @@ export default function Sidebar() {
         <div className="px-4 py-2 mt-6 text-xs font-semibold text-gray-400 uppercase">
           {t('sidebar.sections.others')}
         </div>
-        <ul className="space-y-1">
-          {/* Assuming navItems has "Others" section items as well */}
-          {/* {navItems
-            .filter((item) => item.section === 'others')
-            .map((item) => (
-              <li key={item.name}>
-                <Link
-                  href={item.href}
-                  className={`flex items-center px-4 py-2 space-x-3 rounded-md text-sm font-medium transition-colors duration-200 ${
-                    active === item.name
-                      ? 'bg-blue-100 text-blue-500'
-                      : 'text-gray-600 hover:bg-blue-50 hover:text-blue-500'
-                  }`}
-                  onClick={() => setActive(item.name)}
-                >
-                  {item.icon && <item.icon className="h-5 w-5 text-gray-400" />}
-                  <span>{t(item.label)}</span>
-                </Link>
-              </li>
-            ))} */}
-        </ul>
       </nav>
     </div>
   );
